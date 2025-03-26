dd6cadae84020918248aaa23eda27d7f925dc866
lukadi freddy <fredlukadi53@gmail.com>
2025-03-26 15:56:44 +0100

    Model V

---
+++ resources/views/Posts/index.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="header d-flex justify-content-around mt-5">
+        <div><i class="bi bi-chat-dots fs-2"></i></div>
+        <div><input class="form-control" type="search" placeholder="Rechercher..." aria-label="Search">
+        </div>
+        <div class="d-flex">
+            <div class="px-5"><a href="#" class="text-decoration-none success">Connexion</a></div>
+            <div class="table-ho"><a href="#" class="text-decoration-none">Inscription</a></div>
+        </div>
+    </div>
+    <div class="main mt-5 d-flex justify-content-center p-5">
+        <div class="d-flex border-bottom border-5 p-4 container">
+            <div>
+                <img src="..." class="" alt="...">
+            </div>
+            <div>
+                <h4 class="fw-bold">Categorie</h4>
+                <h2 class="fw-bold">Titre</h2>
+                <div class="d-flex">
+                    <div class="btn btn-danger rounded-pill px-3">Tag 1</div>
+                    <div class="btn btn-danger rounded-pill px-3 mx-1">Tag 2</div>
+                </div>
+                <div class="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit accusantium qui, similique ad at
+                    eos totam neque ipsa fuga accusamus nesciunt voluptatibus modi expedita minus, magnam architecto,
+                    perspiciatis voluptas culpa.
+                </div>
+                <a href="#" class="btn btn-dark mt-3 p-3 rounded-pill"><i class="bi bi-book"></i> Lire article</a>
+            </div>
+        </div>
+    </div>
+
+    </div>
+
+</body>
+
+
+
+
+
+
+
+<style>
+    body {
+        font-family: Arial, Helvetica, sans-serif
+    }
+</style>
+
+</html>
